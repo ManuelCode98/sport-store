499d50c9d2ddd2dc572638e1e7f138fb8e251285
Manuel Rodriguez <cuentapersonal.0098@gmail.com>
2025-12-10 10:02:42 -0500

    Carpeta data recuperada

---
+++ src/data/path.js
@@ -0,0 +1,60 @@
+
+export const enterizos = '/assets/catalogo/enterizos';
+export const buzos = '/assets/catalogo/buzos';
+export const shorts = '/assets/catalogo/shorts';
+export const blusas = '/assets/catalogo/blusas';
+export const blusones = '/assets/catalogo/blusones';
+export const shortMasTop = '/assets/catalogo/short+top';
+export const shortMasCropTop = '/assets/catalogo/short+crop-top';
+export const licraMastop = '/assets/catalogo/licra+top';
+export const licras = '/assets/catalogo/licras';
+export const top = '/assets/catalogo/top';
+export const cropTop = '/assets/catalogo/crop-top';
+export const medias = '/assets/catalogo/medias';
+export const cinturillas = '/assets/catalogo/cinturillas';
+export const bicicleteroMasBuzo = '/assets/catalogo/bicicletero+buzo';
+
+export const basicas = 'basicas';
+export const pushUp = 'push-up';
+export const calentadoras = 'calentadoras';
+export const unicolor = 'unicolor';
+export const enV = 'en-v';
+export const bolsillo = 'bolsillo-suplex';
+export const plus = 'plus';
+export const seamless = 'seamless';
+export const corto = 'corto';
+export const largo = 'largo';
+export const latex = 'latex';
+export const powernet = 'powernet';
+export const premium = 'premium';
+export const suplex = 'suplex';
+export const nylon = 'nylon';
+export const estampado = 'estampado';
+export const texturizado = 'texturizado';
+
+export const serenity = 'serenity';
+export const ela = 'ela';
+export const mangaLarga = 'manga-larga';
+export const martina = 'martina';
+export const oversizeCapriati = 'oversize-capriati';
+export const talego = 'talego';
+export const sinPushUp = 'sin-push-up';
+export const corazon = 'corazon';
+export const picapiedras = 'picapiedras';
+export const angie = 'angie';
+export const luxury = 'luxury';
+export const alejandra = 'alejandra';
+export const aqua = 'aqua';
+export const glamCrochet = 'glam-crochet';
+export const linley = 'linley';
+export const cristina = 'cristina';
+export const naim = 'naim';
+export const karin = 'karin';
+
+
+
+export const rib = 'rib';
+export const libelula = 'libelula';
+export const asimetrica = 'asimetrica';
+export const mora = 'mora';
+export const nudoPoliamida = 'nudo-poliamida';
